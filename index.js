c7ce02b6364821200924d96252ae481ca571dbdb
musk0005 <122370077+musk0005@users.noreply.github.com>
2023-01-24 09:52:21 -0500

    index.js
    java coding

---
+++ index.js
@@ -0,0 +1,50 @@
+//Task 1
+//Declaring variables
+let a = 3;
+let b = 5;
+let c;
+let input;
+let sum = a+b;
+let sub = a-b;
+let mul = a*b;
+let div = a/b;
+let per = a%b;
+let abc = a+=b;
+let def = a-=b;
+let ghi = a*=b;
+let jkl = a/=b;
+let mno = a%=b;
+let pqr = a==b;
+let stw = a!=b;
+
+ input = 'let a = 3;' + "\n";
+ input += 'let b = 5;' + "\n";
+ input += 'let c;' + "\n";
+ input += '----------'
+  alert(input + "\n"
+        +'a + b = ' + sum + "\n" 
+        +'a - b = ' + sub +"\n" 
+        +'a * b = ' + mul +"\n" 
+        +'a / b = ' + div +"\n" 
+        +'a % b = ' + per +"\n" 
+        +'(a += b) = ' +abc +"\n" 
+        +'(a -= b) = ' +def +"\n" 
+        +'(a *= b) = ' +ghi +"\n" 
+        +'(a /= b) = ' +jkl +"\n" 
+        +'(a %= b) = ' +mno +"\n" 
+        +'(a == b) = ' +pqr +"\n" 
+        +'(a != b) = ' +stw);
+
+
+//Task 2
+
+let first_name = 'Muskaan';
+let last_name = 'Bansal';
+let email = 'musk0005@algonquinlive.com' ;
+let output;
+
+output = 'My name is ' + first_name + ' '+ last_name + '. ' + 'You can contact me at ' + email + '.';
+
+alert(output);
+
+
